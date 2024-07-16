c10c5fbd31c574c6c8bf922708a6d55a069841a1
Resul <owezresul0@gmail.com>
2024-07-16 13:10:32 +0500

    test

--- src/ErrElement.jsx
+++ src/ErrElement.jsx
@@ -2,7 +2,7 @@
 function ErrElement() {
     return(
         <>
-            <div className="">Error 4wqsqwssd04@!@!</div>
+            <div className="">Error 4wqsqwssdfgdcgh04@!@!</div>
         </>
     )
 }
