90e8891e2b6eaf5a99ceff4ec35477fbc76233f6
Resul <owezresul0@gmail.com>
2024-07-25 12:58:39 +0500

    change text title in mainpage

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,11 +9,11 @@ function AboutUs() {
                     <span>Кто мы?</span>
                     <div className="about-us-info">
                         <div className="info-item1 info-item">
-                            <p className="info-title">Продукция</p>
+                            {/* <p className="info-title">Продукция</p> */}
                             <p className="info-text">Наши стеклянные чашки – это больше, чем просто посуда, это воплощение утонченного вкуса. Созданные с предельной точностью, каждое изделие представляет собой гармоничное сочетание формы и функциональности.</p>
                         </div>
                         <div className="info-item2 info-item">
-                            <p className="info-title">Команда</p>
+                            {/* <p className="info-title">Команда</p> */}
                             <p className="info-text">Повысьте уровень своих повседневных ритуалов с нашей коллекцией изысканной стеклянной посуды.</p>
                         </div>
                         <div className="info-item3 info-item">
