3f1e2c97befe9f4e5801d7505bb05c626d6042e5
Resul <owezresul0@gmail.com>
2024-07-25 13:04:49 +0500

    change text

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -9,17 +9,19 @@ function AboutUs() {
                     <span>Кто мы?</span>
                     <div className="about-us-info">
                         <div className="info-item1 info-item">
-                            {/* <p className="info-title">Продукция</p> */}
-                            <p className="info-text">Наши стеклянные чашки – это больше, чем просто посуда, это воплощение утонченного вкуса. Созданные с предельной точностью, каждое изделие представляет собой гармоничное сочетание формы и функциональности.</p>
+                            <p className="info-title">Изящные линии, чистый вкус.</p>
+                            <p className="info-text">Наши стеклянные чашки – это больше, чем просто посуда, это воплощение утонченного вкуса.</p>
                         </div>
                         <div className="info-item2 info-item">
-                            {/* <p className="info-title">Команда</p> */}
+                            <p className="info-title">Подчеркните красоту вашего стола.</p>
                             <p className="info-text">Повысьте уровень своих повседневных ритуалов с нашей коллекцией изысканной стеклянной посуды.</p>
                         </div>
                         <div className="info-item3 info-item">
+                            <p className="info-title">Идеальный подарок для себя и близких.</p>
                             <p className="info-text">Превратите каждый момент в особенный. Наши стеклянные стаканы добавят изюминку в вашу повседневную жизнь.</p>
                         </div>
                         <div className="info-item4 info-item">
+                            <p className="info-title">Cделано с любовью.</p>
                             <p className="info-text">Наслаждайтесь любимыми напитками в окружении красоты и элегантности.</p>
                         </div>
                     </div>
