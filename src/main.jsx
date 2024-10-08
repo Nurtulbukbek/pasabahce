5fbcf3323b81f97447f1d65c78ed18c90cd72329
Resul <owezresul0@gmail.com>
2024-10-08 19:02:27 +0500

    fix2

--- src/main.jsx
+++ src/main.jsx
@@ -124,7 +124,7 @@ import Frigo80050 from './catalogFilesPhotos/DecanterPhotos/Frigo80050.jsx'
 import Basic43964 from './catalogFilesPhotos/DecanterPhotos/Basic43964.jsx'
 
 // ----------tea---------
-import Tea97948 from './catalogFilesPhotos/teaFolder/Tea97948.jsx'
+import Tea97948 from "src/catalogFilesPhotos/teaFolder/Tea97948.jsx"
 // import Chroma55813 from './catalogFilesPhotos/teaFolder/Chroma55813.jsx'
 // import Tashkent96806 from './catalogFilesPhotos/teaFolder/Tashkent96806.jsx'
 // import TeaCoffee55411 from './catalogFilesPhotos/teaFolder/TeaCoffee55411.jsx'
